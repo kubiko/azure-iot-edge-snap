1a0e081728065d97af0344549d73246281155607
Ondrej Kubik <ondrej.kubik@canonical.com>
2020-09-07 18:07:25 +0100

    snap: hooks: configure: move handling of connection string to iotedge wrapper
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- local-assets/bin/iotedged-wrapper.sh
+++ local-assets/bin/iotedged-wrapper.sh
@@ -1,17 +1,42 @@
 #!/usr/bin/env bash
+
 set -Eeuo pipefail
 
+wait_for_dockerd() {
+	# wait till dockerd starts, max wait 10 seconds!
+	local counter=0
+	while [ ! -e /run/docker.sock ] && [ ${counter} -lt 10 ];
+	do
+		echo "Waiting for dockerd to start"
+		sleep 1
+		let counter=counter+1
+	done
+}
+
+update_connection_string() {
+	local cs=$(snapctl get connection-string)
+	if [ -n "${cs}" ]; then
+		${SNAP}/bin/yq w -i \
+			${SNAP_DATA}/etc/iotedge/config.yaml \
+			  provisioning.device_connection_string --style=double "${cs}"
+
+		# unset string from snap config
+		snapctl unset connection-string
+	fi
+}
+
+update_host_name() {
 # update device host name in iotedge config.yaml
-sed -i -e \
-	's/hostname: .*/hostname: \"'"${HOSTNAME}"'\"/g' \
-	${SNAP_DATA}/etc/iotedge/config.yaml
+	${SNAP}/bin/yq w -i \
+		${SNAP_DATA}/etc/iotedge/config.yaml \
+		hostname --style=double "${HOSTNAME}"
+}
 
+# update config if needed
+update_host_name
+update_connection_string
+# wait for dockerd
+wait_for_dockerd
 
-# wait till dockerd starts
-while [ ! -e /run/docker.sock ];
-do
-	echo "Waiting for dockerd to start"
-	sleep 1
-done
 exec "$@" \
-    -c ${SNAP_DATA}/etc/iotedge/config.yaml
+    -c /etc/iotedge/config.yaml
